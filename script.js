3cae6ac27a3425cfd83ff93e42c8251c952eb226
slush1616 <assassinyt@gmail.com>
2023-09-13 18:12:47 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-bl");
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 150 + "px";
+  blur.style.top = dets.y - 150 + "px";
+});
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "70px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    start: "top -10%",
+    end: "top 11%",
+    scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
